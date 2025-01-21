bef03062edee749d88278b531281a26a0e10caee
Kishore S <152197053+DonGaruda@users.noreply.github.com>
2025-01-21 21:24:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from scikit-learn.model_selection import train_test_split
+from scikit-learn.preprocessing import StandardScaler
+from scikit-learn.ensemble import RandomForestClassifier
+from scikit-learn.metrics import accuracy_score, classification_report
 import streamlit as st
 import plotly.express as px
 
